cadb2a61ade61fc79cdee46a40ed60efb47dcfec
Christian Hardy <ChristianTH.to@gmail.com>
2022-12-31 17:56:22 -0500

    Update classification-and-generation.js

--- classification-and-generation.js
+++ classification-and-generation.js
@@ -17,13 +17,15 @@
         // Use the GPT-3 model to generate text based on the classification
         const response = await openai.textGeneration.create({
               
-          prompt: `You are an AI mental health assistant that can have meaningful conversations with people. 
-                   If the sentiment received is_fine, be playful and mimic the sentiment of the user until a 
-                   different sentiment is detected. If the sentiment is assess_further, have a conversaion, ask the 
-                   user questions and mimic their sentiment(if they are sarcastic be sarcastic) until a positive 
-                   or negative sentiment is detected based on the classification input from each response. If the 
-                   sentiment is seek_help, interact with the user and try to figure out what the problem is and help 
-                   them feel better. The sentiment of the transcription is ${classification}.`,
+          prompt: `I want you to act as a mental health assistant that can have meaningful conversations. 
+                   Use your knowledge of cognitive behavioral therapy, jungian therapy, and other theraputic 
+                   methods to help guide people to different strategies to improve their wellbeing. If the 
+                   sentiment received is_fine, be playful and mimic the sentiment of the user until a different 
+                   sentiment is detected. If the sentiment is assess_further, use your wit, creativity, and 
+                   observational skills to have a conversaion. Ask the user questions and mimic their sentiment 
+                   until a positive or negative sentiment is detected based on the classification input from each 
+                   response. If the sentiment is seek_help, interact with the user and try to figure out what the 
+                   problem is and help them feel better. The sentiment of the transcription is ${classification}.`,
               
           model: 'text-davinci-003',
           temperature: 0.5,
