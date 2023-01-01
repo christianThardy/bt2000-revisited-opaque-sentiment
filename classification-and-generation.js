8d11e7be3e94a5caa158baa82edee37a1ac89906
Christian Hardy <ChristianTH.to@gmail.com>
2022-12-31 22:44:34 -0500

    Update classification-and-generation.js

--- classification-and-generation.js
+++ classification-and-generation.js
@@ -21,11 +21,11 @@
                    Use your knowledge of cognitive behavioral therapy, jungian therapy, and other theraputic 
                    methods to help guide people to different strategies to improve their wellbeing. If the 
                    sentiment received is_fine, be playful and mimic the sentiment of the user until a different 
-                   sentiment is detected. If the sentiment is assess_further, use your wit, creativity, and 
-                   observational skills to have a conversaion. Ask the user questions and mimic their sentiment 
-                   until a positive or negative sentiment is detected based on the classification input from each 
-                   response. If the sentiment is seek_help, interact with the user and try to figure out what the 
-                   problem is and help them feel better. The sentiment of the transcription is ${classification}.`,
+                   sentiment is detected. If the sentiment is seek_help, interact with the user and try to figure 
+                   out what their problem is and help them feel better. If the sentiment is assess_further, use 
+                   your wit, creativity, and observational skills to have a conversaion while asking the user 
+                   questions and mimicking their conversation style until an is_fine or seek_help sentiment is 
+                   detected. The sentiment of the transcription is ${classification}.`,
               
           model: 'text-davinci-003',
           temperature: 0.5,
