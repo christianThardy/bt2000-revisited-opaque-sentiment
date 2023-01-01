b8d4efb6bd6d92081251f622da2199273082f3f7
Christian Hardy <ChristianTH.to@gmail.com>
2023-01-01 15:25:13 -0500

    Create whisper-speech-recognition.js

---
+++ whisper-speech-recognition.js
@@ -0,0 +1,23 @@
+<script src="https://unpkg.com/@openai/whisper@latest/dist/whisper.min.js"></script>
+
+    <script>
+      const apiKey = process.env.OPENAI_KEY;
+      const whisper = new Whisper({
+        apiKey: apiKey
+      });
+
+
+      whisper.on('transcription', async transcription => {
+        translatedTextElement.textContent = transcription;
+
+        // Send the transcription to the sentiment classifier
+        // and get the prediction
+        const classification = await classifySentiment(transcription);
+
+        // Send the classification to the GPT-3 model
+        // and get the generated text
+        const generatedText = await generateText(classification);
+        
+        // Update the generated text element with the generated text
+        generatedTextElement.textContent = generatedText;
+      });
